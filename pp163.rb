c2528815dfbf4856b0981a09c12a60e7bf003cd4
Jie Ma <codejma@gmail.com>
2014-01-08 17:33:46 +1100

    pp.163.com; usage: ruby pp163.rb url

---
+++ pp163.rb
@@ -0,0 +1,26 @@
+require 'nokogiri'
+require 'open-uri'
+require 'fileutils'
+
+# Get a Nokogiri::HTML::Document for the page we’re interested in...
+url = ARGV[0]
+doc = Nokogiri::HTML(open(url))
+
+title = doc.css('h2#p_username_copy').first.text.delete("\t").delete("\r").delete("\n").strip
+p title
+
+authorid = url.match(/163.com\/(.+)\/pp/)[1]
+p authorid
+
+folder = File.join(File.dirname(__FILE__), '163_' + authorid , '163_' + authorid + '_' + title)
+p folder
+
+FileUtils::mkdir_p folder
+
+doc.css('div.pic-area img.z-tag').each do |img|
+  uri = img['data-lazyload-src']
+  p uri
+  File.open(File.join(folder, File.basename(uri)), 'wb') { |f| f.write(open(uri).read)}
+end
+
+p 'done'
